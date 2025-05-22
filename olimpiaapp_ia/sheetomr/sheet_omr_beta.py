61fcccf32c456305880544194e331402891a3706
Leonardo Fabyan <leofabyano@gmail.com>
2025-05-21 18:32:31 -0600

    Add Pylance typing

--- olimpiaapp_ia/sheetomr/sheet_omr_beta.py
+++ olimpiaapp_ia/sheetomr/sheet_omr_beta.py
@@ -1,14 +1,18 @@
 from imutils.perspective import four_point_transform
+# Nota: Leo, 21/05/2025, 6:10 pm
+# >> from imutils import contours
+# Si no se va a usar "contours", entonces es mejor no importar "from imutils import contours"
 from imutils import contours
 import numpy as np
 import imutils
 import cv2
 
-def readImage(localTestImagePath):
-    image = cv2.imread(localTestImagePath)
-    return image
 
-def edgeDetection(image):
+def readImage(image_path: str) -> np.ndarray[tuple[int, int, int], np.dtype[np.uint8]]:
+    return cv2.imread(image_path) # type: ignore
+
+def edgeDetection(image: np.ndarray[tuple[int, int, int], np.dtype[np.uint8]]) -> tuple[np.ndarray[tuple[int, int], np.dtype[np.uint8]],
+                                                                                        np.ndarray[tuple[int, int], np.dtype[np.uint8]]]:
     #Convertir a escala de grises y eliminar ruido
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     blurred = cv2.GaussianBlur(gray, (5, 5), 0)
@@ -16,12 +20,15 @@ def edgeDetection(image):
     img_dilation = cv2.dilate(blurred, kernel, iterations=1)  
     edged = cv2.Canny(img_dilation, 75, 200)
     
-    return edged, gray
+    return edged, gray # type: ignore
 
-def findContours(edges_in_image, gray_image, image):
+def findContours(edges_in_image: np.ndarray[tuple[int, int], np.dtype[np.uint8]], 
+                 gray_image: np.ndarray[tuple[int, int], np.dtype[np.uint8]], 
+                 image: np.ndarray[tuple[int, int, int], np.dtype[np.uint8]]) -> tuple[np.ndarray[tuple[int, int], np.dtype[np.uint8]],
+                                                                                       np.ndarray[tuple[int, int, int], np.dtype[np.uint8]]]:
     # Encontrar contornos
     cnts = cv2.findContours(edges_in_image.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cnts=imutils.grab_contours(cnts)
+    cnts = imutils.grab_contours(cnts)
     docCnt = None
     
     #Asegurarse que se encontro al menos un contorno
@@ -49,30 +56,33 @@ def findContours(edges_in_image, gray_image, image):
         thresh = cv2.threshold(warped.copy(), 0, 255, cv2.THRESH_BINARY_INV | 
                        cv2.THRESH_OTSU)[1]
 
-        return thresh, four_point_view_paper
-    else:
-	    #Si no se detecta el borde de la hoja (el contenido abarca toda la imagen),
-	    #se usa el borde de la imagen
-        h, w = image.shape[:2]
-        docCnt = np.array([
-            [[0, 0]],         # Esquina superior izquierda
-            [[w-1, 0]],       # Esquina superior derecha
-            [[w-1, h-1]],     # Esquina inferior derecha
-            [[0, h-1]]        # Esquina inferior izquierda
-        ])
-	    #Dibujar el contorno detectado
-        cv2.drawContours(image, [docCnt], -1, (0, 255, 0), 2)
-        four_point_view_paper=four_point_transform(image, docCnt.reshape(4, 2))
-        warped = four_point_transform(gray_image, docCnt.reshape(4, 2))
-        thresh = cv2.threshold(warped.copy(), 0, 255, cv2.THRESH_BINARY_INV | 
-                       cv2.THRESH_OTSU)[1]
-        return thresh, four_point_view_paper
+        return thresh, four_point_view_paper # type: ignore
+    
+    #Si no se detecta el borde de la hoja (el contenido abarca toda la imagen),
+	#se usa el borde de la imagen
+    h, w = image.shape[:2]
+    docCnt = np.array([
+        [[0, 0]],         # Esquina superior izquierda
+        [[w - 1, 0]],       # Esquina superior derecha
+        [[w - 1, h - 1]],     # Esquina inferior derecha
+        [[0, h - 1]]        # Esquina inferior izquierda
+    ])
+	#Dibujar el contorno detectado
+    cv2.drawContours(image, [docCnt], -1, (0, 255, 0), 2)
+    four_point_view_paper=four_point_transform(image, docCnt.reshape(4, 2))
+    warped = four_point_transform(gray_image, docCnt.reshape(4, 2))
+    thresh = cv2.threshold(warped.copy(), 0, 255, cv2.THRESH_BINARY_INV | 
+                           cv2.THRESH_OTSU)[1]
 
-def cropImage(thresh_img, orig_img):
-    top_fraction=0.12
-    bottom_fraction=0.04
-    left_fraction=0.05
-    right_fraction=0.05
+    return thresh, four_point_view_paper # type: ignore
+
+def cropImage(thresh_img: np.ndarray[tuple[int, int], np.dtype[np.uint8]], 
+              orig_img: np.ndarray[tuple[int, int, int], np.dtype[np.uint8]]) -> tuple[np.ndarray[tuple[int, int], np.dtype[np.uint8]],
+                                                                                       np.ndarray[tuple[int, int, int], np.dtype[np.uint8]]]:
+    top_fraction = 0.12
+    bottom_fraction = 0.04
+    left_fraction = 0.05
+    right_fraction = 0.05
     height, width = thresh_img.shape[:2]
     
     #Calcular los pixeles a cortar
@@ -84,23 +94,24 @@ def cropImage(thresh_img, orig_img):
     cropped_image_thresh = thresh_img[top:bottom, left:right]
     cropped_image_org = orig_img[top:bottom, left:right]
 
-    return cropped_image_thresh, cropped_image_org 
+    return cropped_image_thresh, cropped_image_org  # type: ignore
    
-def findBubbles(thresh_img, paper):
-    thresh_copy=thresh_img.copy()
+def findBubbles(thresh_img: np.ndarray[tuple[int, int], np.dtype[np.uint8]], 
+                paper: np.ndarray[tuple[int, int, int], np.dtype[np.uint8]]) -> np.ndarray[tuple[int, int, int], np.dtype[np.uint8]] | None:
+    thresh_copy = thresh_img.copy()
     cnts = cv2.findContours(thresh_copy, cv2.RETR_TREE, 
                             cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
     questionCnts = []
     for c in cnts:
-        (x,y,w,h) = cv2.boundingRect(c)
+        x, y, w, h = cv2.boundingRect(c)
         ar = w/float(h)
         
         if 20 <= w <= 70 and 20 <= h <= 70 and 0.7 <= ar <= 1.2:
             questionCnts.append(c)
 
-    if len(questionCnts)>0:
-        bubbles_paper=paper.copy()
+    if len(questionCnts) > 0:
+        bubbles_paper = paper.copy()
         cv2.drawContours(bubbles_paper, questionCnts, -1, (0, 0, 255), 2)
  
         return bubbles_paper
